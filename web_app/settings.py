73d76cb9e4c5da0234b3dafef0db655d1106ea7d
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-05-06 15:59:26 +0000

    init

--- web_app/settings.py
+++ web_app/settings.py
@@ -147,11 +147,10 @@ STATIC_URL = '/static/'
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 's_files'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
