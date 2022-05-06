c31f69dd2117b80b3c60e07b7da50f5ce1d3e99e
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-05-06 15:14:12 +0000

    make it better

--- web_app/settings.py
+++ web_app/settings.py
@@ -142,13 +142,17 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
 
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 's_files')
-]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 's_files')
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 's_files'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
