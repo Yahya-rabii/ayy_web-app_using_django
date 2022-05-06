b29d14141279d3b9e9ba730f6b8ea2aecf4e66ca
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-05-06 13:20:13 +0000

    init

--- web_app/settings.py
+++ web_app/settings.py
@@ -29,14 +29,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['yaycycling.herokuapp.com','127.0.0.1']
 
-
+"""
+"""
 SECURE_HSTS_SECONDS = 2_592_000
 SECURE_SSL_REDIRECT =True
 SECURE_HSTS_INCLUDE_SUBDOMAINS=True
 SESSION_COOKIE_SECURE=True
 SECURE_HSTS_PRELOAD=True
 CSRF_COOKIE_SECURE=True
-
 
 # Application definition
 
@@ -151,8 +151,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 's_files')
 ]
-
-STATIC_ROOT =[
-os.path.join(BASE_DIR, 's_files')
-] 
 
