eef4d30194f61a04fe7ae61da4e3235d8d8c1bf2
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-05-06 17:16:27 +0000

    init

--- web_app/settings.py
+++ web_app/settings.py
@@ -144,13 +144,14 @@ USE_TZ = True
 
 
 
-
 STATIC_URL = '/static/'
 
-if not DEBUG: 
-    
-    STATIC_ROOT = os.path.join(BASE_DIR, 's_files')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 's_files'),
+)
+
