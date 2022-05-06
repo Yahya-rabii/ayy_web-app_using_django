5d244024c7e96a3220d0f18897039f3b2bf8217f
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-05-06 22:34:16 +0000

    init

--- web_app/settings.py
+++ web_app/settings.py
@@ -68,7 +68,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
