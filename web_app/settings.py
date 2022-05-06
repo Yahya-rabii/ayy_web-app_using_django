a8501b2661e218e347e9b06217cc7d9a5832105a
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-05-06 17:24:53 +0000

    init

--- web_app/settings.py
+++ web_app/settings.py
@@ -142,14 +142,24 @@ USE_L10N = True
 USE_TZ = True
 
 
-
-
+"""
 STATIC_URL = '/static/'
 
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 's_files'),
+)
+"""
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 's_files'),
