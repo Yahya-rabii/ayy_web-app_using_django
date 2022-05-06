30e5ee13df3e12cc4f7a0ed1564629790207904f
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-05-06 03:55:21 +0000

    init

--- web_app/settings.py
+++ web_app/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['yaycycling.herokuapp.com','127.0.0.1']
 
-""""""
+
 SECURE_HSTS_SECONDS = 2_592_000
 SECURE_SSL_REDIRECT =True
 SECURE_HSTS_INCLUDE_SUBDOMAINS=True
