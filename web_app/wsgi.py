43e581f8c0fd8289529bb9a1f91bc5a6231a450d
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-05-06 17:54:58 +0000

    init

--- web_app/wsgi.py
+++ web_app/wsgi.py
@@ -15,9 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_app.settings')
 
 application = get_wsgi_application()
 
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise import DjangoWhiteNoise
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
