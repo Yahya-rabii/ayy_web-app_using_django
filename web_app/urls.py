f13770c06bd10ec302b1b2f8abddb0eaf1bdea23
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-05-06 22:25:58 +0000

    init

--- web_app/urls.py
+++ web_app/urls.py
@@ -42,7 +42,6 @@ urlpatterns = [
     path('user_login/', views.login),
 
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     ]
 
