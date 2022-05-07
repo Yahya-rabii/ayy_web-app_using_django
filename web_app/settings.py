e01a2f29fe6a4739a2d8e8c5f0f3b141be65947a
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-05-07 00:56:46 +0000

    init

--- web_app/settings.py
+++ web_app/settings.py
@@ -173,22 +173,3 @@ LOGGING = {
         },
     },
 }
-
-
-
-
-import dj_database_url
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    print("Postgres URL not found, using sqlite instead")
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
